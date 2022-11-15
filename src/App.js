db5aee5752092cc9b5845241bb3493e16b34c5aa
thubuiFE <thu.bui@executionlab.asia>
2022-11-15 17:10:12 +0700

    config redux-toolkit and redux-persist

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import SectionSobre from "./mains/SectionSobre";
 import SectionContact from "./mains/SectionContact";
 import Footer from "./mains/Footer";
 import { LocalProvider } from "./contexts/LocaleContext";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { persistor, store } from "./store";
 
 function App() {
   return (
-    <div className="doctor-care-wrapper">
-      <LocalProvider>
-        <TopHeader />
-      </LocalProvider>
-      <SectionServicos />
-      <SectionSobre />
-      <SectionContact />
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <div className="doctor-care-wrapper">
+          <LocalProvider>
+            <TopHeader />
+          </LocalProvider>
+          <SectionServicos />
+          <SectionSobre />
+          <SectionContact />
+          <Footer />
+        </div>
+      </PersistGate>
+    </Provider>
   );
 }
 
--- src/molecules/HeaderMenu/index.jsx
+++ src/molecules/HeaderMenu/index.jsx
@@ -1,41 +1,49 @@
 // libs
-import React, { useState } from "react";
-// dataSources
-import { dataMenu } from "../../dataSources/menuData";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 // context
 import { useLocale } from "../../contexts/LocaleContext";
+import { fetchDataMenu } from "../../reducer/fetchDataMenu";
 // styles
 import "./styles.scss";
 
 const HeaderMenu = ({ color }) => {
   const [itemSelected, setItemSelected] = useState(0);
   const { localeDataSource } = useLocale();
+  const dispatch = useDispatch();
+  const dataMenu = useSelector((state) => state?.fetchData?.dataMenu);
+
+  useEffect(() => {
+    dispatch(fetchDataMenu());
+  }, []);
 
   return (
-    <div className="header-menu-wrapper">
-      <ul>
-        {dataMenu?.map((item) => (
-          <a
-            href="#"
-            key={item?.key}
-            onClick={() => setItemSelected(item?.key)}
-          >
-            <li
-              className={item?.key === itemSelected ? "li-selected" : ""}
-              style={{ color }}
+    dataMenu && (
+      <div className="header-menu-wrapper">
+        <ul>
+          {dataMenu?.map((item) => (
+            <a
+              href="#"
+              key={item?.key}
+              onClick={() => setItemSelected(item?.key)}
             >
-              {item?.key === itemSelected ? (
-                <span style={{ borderColor: color }}>
-                  {localeDataSource[item?.title]}
-                </span>
-              ) : (
-                localeDataSource[item?.title]
-              )}
-            </li>
-          </a>
-        ))}
-      </ul>
-    </div>
+              <li
+                className={item?.key === itemSelected ? "li-selected" : ""}
+                style={{ color }}
+              >
+                {item?.key === itemSelected ? (
+                  <span style={{ borderColor: color }}>
+                    {localeDataSource[item?.title]}
+                  </span>
+                ) : (
+                  localeDataSource[item?.title]
+                )}
+              </li>
+            </a>
+          ))}
+        </ul>
+      </div>
+    )
   );
 };
 
